f2d24a539f4de82051837fd3116465656dd24036
slemusbelatrix <slemus@belatrixsf.com>
2019-02-26 09:47:28 -0500

    cleaning code

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 import './index.css';
 import Home from './pages/Home';
 import reducers from './reducers';
-import * as serviceWorker from './serviceWorker';
 
 
 const store = createStore(reducers, applyMiddleware(thunk));
@@ -25,8 +24,4 @@ ReactDOM.render(
   </Provider>
 
 , document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
